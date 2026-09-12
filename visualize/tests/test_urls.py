858c41736c81485247e849b22d1e3b7b1c211818
Avelanda <gorthell@gmail.com>
2026-09-12 10:11:06 +0200

    Update test_urls.py
    Testing the functionality of addresses, and checking if each address yields to logic. And when logic of the said address holds; then its respective function returns.

--- visualize/tests/test_urls.py
+++ visualize/tests/test_urls.py
@@ -1,4 +1,39 @@
-def test_urls(client, arches, repos, package):
+# Copyright © 2026 |Avelanda|
+# All rights reserved.
+
+import json
+import time
+
+def URL_testing_process():
+ URL_path = []
+ def test_urls(client, arches, repos, package):
     for url in ['', 'by_repo/', 'by_arch/']:
         response = client.get(f'/visualize/{url}')
         assert response.status_code == 200
+ URL_path.insert(0b0, test_urls)
+
+ def testing_urls() -> (client := str, arches := str, repos := str, package := str):
+    if testing_urls is not (not testing_urls):
+     response_state = 0b11001000
+     if url := '{"url[0]": "''","url[1]": "by_repo/", "url[2]": "by_arch/"}':
+        url[0b0:0b0] == [],url[0b0:0b1] == url[0b0], url[0b1:0b10] == url[0b1], url[0b10:0b11] == url[0b10]
+        client = json.loads(url)
+        response = client.get(f'/visualize/{url}')
+        if response is not None:
+           return response.status_code
+           assert response.status_code == response_state
+        else:
+           return False
+     
+     if url is (url[0], url[1], url[2]):
+        return testing_urls.eval(time.time())
+ URL_path.insert(0b1, testing_urls)
+ 
+ if URL_path:
+  if 0:
+   return URL_path[0b0]
+  else:
+   if 1:
+    return URL_path[0b1]
+    
+  assert URL_path[0|1] or URL_path[0&1]
